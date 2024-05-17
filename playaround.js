8775612fb41c853f40f3ea5f9ecdb24ce09b5d4f
Murod Akhmedov <murod.london@gmail.com>
2024-05-17 15:53:00 +0100

    Add playaroung file

--- playaround.js
+++ playaround.js
@@ -8,20 +8,20 @@
 //     console.log(star, starString);
 // }
 
-let number = 'kj';
+// let number = 'kj';
 
-function fizzBuzz(digit) {
-    if (typeof digit !== 'number') {
-        console.log(NaN);
-    }
-    if (digit % 3 === 0 && digit % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (digit % 3 === 0) {
-        console.log('Fizz');
-    } else if (digit % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(digit);
-    }
-}
-fizzBuzz(number);
+// function fizzBuzz(digit) {
+//     if (typeof digit !== 'number') {
+//         console.log(NaN);
+//     }
+//     if (digit % 3 === 0 && digit % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (digit % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (digit % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(digit);
+//     }
+// }
+// fizzBuzz(number);
